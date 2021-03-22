6147f2f026f5822ffb59e4398bcbc584eff957f1
Maartje <amvankranen@gmail.com>
2021-03-22 10:08:56 +0100

    feat(blocks): added HTML, made announcements dynamic

--- app/public/wp-content/themes/bredaserc/blocks/announcements.php
+++ app/public/wp-content/themes/bredaserc/blocks/announcements.php
@@ -1,1 +1,26 @@
-Hey ik ben het Announcements blok
+<?php if( have_rows('announcements_content') ) : ?>
+    <section class="section section--grey announcements">
+        <div class="container">
+            <div class="columns">
+                <?php while( have_rows('announcements_content') ) : the_row(); 
+                $image = get_sub_field('announcements_content_img');
+                $button = get_sub_field('announcements_content_btn'); ?>
+                    <div class="column is-12 is-6-desktop">
+                        <div class="card announcement__card">
+                            <div class="card__visual">
+                                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" title="<?php echo $image['title']; ?>">
+                                <?php if (isset($button) && !empty($button)) : ?>
+                                    <a href="<?php echo $button['url']; ?>" target="<?php echo $button['target']; ?>" class="button button--primary"><?php echo $button['title']; ?></a>
+                                <?php endif; ?>
+                            </div>
+                            <div class="card__text">
+                                <h2 class="is-size-4 is-size-4-desktop"><?php echo get_sub_field('announcements_content_title'); ?></h2>
+                                <p><?php echo get_sub_field('announcements_content_text'); ?></p>
+                            </div>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            </div>
+        </div>
+    </section>
+<?php endif; 
--- app/public/wp-content/themes/bredaserc/blocks/slider.php
+++ app/public/wp-content/themes/bredaserc/blocks/slider.php
@@ -1,1 +1,33 @@
-Ik ben de slider!
+<section class="section sponsors">
+        <div class="container">
+            <h2 class="is-size-2">Onze sponsoren</h2>
+            <div class="columns">
+                <div class="column is-10 is-offset-1">
+                    <div class="glider-contain multiple">
+                        <button class="glider-prev" aria-label="Previous">
+                            <i class="fa fa-chevron-left"></i>
+                        </button> 
+
+                        <div class="glider">
+                            <img src="https://via.placeholder.com/250x250" alt="">
+                            <img src="https://via.placeholder.com/250x250" alt="">
+                            <img src="https://via.placeholder.com/250x250" alt="">
+                            <img src="https://via.placeholder.com/250x250" alt="">
+                            <img src="https://via.placeholder.com/250x250" alt="">
+                            <img src="https://via.placeholder.com/250x250" alt="">
+                            <img src="https://via.placeholder.com/250x250" alt="">
+                            <img src="https://via.placeholder.com/250x250" alt="">
+                            <img src="https://via.placeholder.com/250x250" alt="">
+                        </div>
+
+                        <button class="glider-next" aria-label="Next">
+                            <i class="fa fa-chevron-right"></i>
+                        </button>
+
+                        <div id="dots" class="glider-dots"></div>
+                    </div>
+                </div>
+            </div>
+            <button class="button button--primary">Ook onze club steunen?</button>
+        </div>
+    </section>

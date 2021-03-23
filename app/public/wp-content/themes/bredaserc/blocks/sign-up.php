305a17846b48b933dded256c42dabff7e8931cef
Maartje <amvankranen@gmail.com>
2021-03-23 18:00:02 +0100

    feat(sign-up): Add correct shortcode to form block

--- app/public/wp-content/themes/bredaserc/blocks/sign-up.php
+++ app/public/wp-content/themes/bredaserc/blocks/sign-up.php
@@ -2,59 +2,16 @@
         <div class="container">
             <div class="columns is-vcentered">
                 <div class="column is-6">
-                    <h2 class="is-size-3-mobile is-size-2">Wil jij ook een rugbyer worden?</h2>
+                    <h2 class="is-size-3-mobile is-size-2"><?php echo get_sub_field('sign-up_title'); ?></h2>
 
                     <div class="sign-up__terms">
-                        <h4 class="is-size-4">Wij regelen het volgende voor je</h3>
-                        <ul>
-                            <li>Je eerste bitje, veiligheid voor alles</li>
-                            <li>Enthousiaste trainers</li>
-                            <li>Gezellige teamgenoten</li>
-                        </ul>
-                        <p>Eerste 6 weken mag je gratis meetrainen!</p>
+                        <?php echo get_sub_field('sign-up_text'); ?>
                     </div>
 
                     <div class="sign-up__form">
-                        <form id="sign-up">
-                            <h3 class="is-size-4-mobile is-size-3">Ja, ik meld me aan</h3>
-                            <div class="field">
-                                <label class="label">Naam</label>
-                                <div class="control">
-                                  <input class="input" type="text" placeholder="Sonny Bill Williams">
-                                </div>
-                              </div>
-                              <div class="field">
-                                <label class="label">Geslacht</label>
-                                <div class="control">
-                                  <input class="input" type="text" placeholder="Man">
-                                </div>
-                              </div>
-                              <div class="field">
-                                <label class="label">Geboortedatum</label>
-                                <div class="control">
-                                  <input class="input" type="text" placeholder="03-08-1985">
-                                </div>
-                              </div>
-                              <div class="field">
-                                <label class="label">Email</label>
-                                <div class="control">
-                                  <input class="input" type="email" placeholder="swilliams@allblacks.com">
-                                </div>
-                                <div class="field">
-                                    <div class="control">
-                                        <label for="" class="checkbox">
-                                            <input type="checkbox">
-                                            Ik accepteer de privacy policy
-                                        </label>
-                                    </div>
-                                </div>
-                              </div>
-
-                              <div class="control">
-                                <button class="button button--primary">Aanmelden</button>
-                              </div>
-                        </form>
+                        <?php echo do_shortcode('[ninja_form id=' . get_sub_field('sign-up_form') . ']'); ?>
                     </div>
+                  
                 </div>
                 <div class="column is-6">
                     <img src="https://source.unsplash.com/1600x900/?rugby" alt="Rugby!">
